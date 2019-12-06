8294d3295a208a6cf133cf268caf895414b059c4
Ivan Boldyrev <lispnik@gmail.com>
2019-12-06 14:23:56 +0700

    Move some reading code from stream.rs to records.rs

--- src/records.rs
+++ src/records.rs
@@ -1,6 +1,7 @@
 use crate::decl::*;
 use byteorder::{NativeEndian, NetworkEndian, ReadBytesExt};
-use std::convert::TryInto;
+use std::collections::HashMap;
+use std::convert::{Into, TryFrom, TryInto};
 use std::io::Read;
 
 #[derive(Clone, Copy, Debug)]
@@ -182,5 +183,212 @@ pub(crate) fn read_0a_start_thread<T: Read>(
 pub(crate) fn read_0b_end_thread<T: Read>(stream: &mut T) -> Result<EndThreadRecord, Error> {
     Ok(EndThreadRecord {
         thread_serial: stream.read_u32::<NetworkEndian>()?,
+    })
+}
+
+pub(crate) fn read_class_description<R: Read>(
+    substream: &mut R,
+    id_reader: IdReader,
+) -> Result<ClassDescription, Error> {
+    let class_id: Id = id_reader.read_id(substream)?;
+    let stack_trace_serial: SerialNumber = substream.read_u32::<NetworkEndian>()?;
+    let super_class_object_id: Id = id_reader.read_id(substream)?;
+    let class_loader_object_id: Id = id_reader.read_id(substream)?;
+    let signers_object_id: Id = id_reader.read_id(substream)?;
+    let protection_domain_object_id = id_reader.read_id(substream)?;
+    let reserved1 = id_reader.read_id(substream)?;
+    let reserved2 = id_reader.read_id(substream)?;
+
+    let instance_size: u32 = substream.read_u32::<NetworkEndian>()?;
+
+    let const_pool_size: u16 = substream.read_u16::<NetworkEndian>()?;
+    let mut const_fields = Vec::with_capacity(const_pool_size as usize);
+    for _idx in 0..const_pool_size {
+        let const_pool_idx: u16 = substream.read_u16::<NetworkEndian>()?;
+        let const_type: FieldType =
+            FieldType::try_from(substream.read_u8()?).or(Err(Error::InvalidField("ty")))?;
+        let const_value = read_type_value(substream, const_type, id_reader)?;
+
+        const_fields.push((
+            ConstFieldInfo {
+                const_pool_idx,
+                const_type,
+            },
+            const_value,
+        ));
+    }
+
+    let static_field_num: u16 = substream.read_u16::<NetworkEndian>()?;
+    let mut static_fields = Vec::with_capacity(static_field_num as usize);
+    for _idx in 0..static_field_num {
+        let name_id: Id = id_reader.read_id(substream)?;
+        let field_type: FieldType =
+            FieldType::try_from(substream.read_u8()?).or(Err(Error::InvalidField("ty")))?;
+        let field_value = read_type_value(substream, field_type, id_reader)?;
+
+        static_fields.push((
+            FieldInfo {
+                name_id,
+                field_type,
+            },
+            field_value,
+        ));
+    }
+
+    let instance_fields_num: u16 = substream.read_u16::<NetworkEndian>()?;
+    let mut instance_fields = Vec::with_capacity(instance_fields_num as usize);
+    for _idx in 0..instance_fields_num {
+        let name_id: Id = id_reader.read_id(substream)?;
+        let field_type: FieldType =
+            FieldType::try_from(substream.read_u8()?).or(Err(Error::InvalidField("ty")))?;
+        instance_fields.push(FieldInfo {
+            name_id,
+            field_type,
+        });
+    }
+
+    Ok(ClassDescription {
+        class_id,
+        stack_trace_serial,
+        super_class_object_id,
+        class_loader_object_id,
+        signers_object_id,
+        protection_domain_object_id,
+        reserved1,
+        reserved2,
+
+        instance_size,
+
+        const_fields,
+        static_fields,
+        instance_fields,
+    })
+}
+
+pub(crate) fn read_object<R: Read>(
+    stream: &mut R,
+    id_reader: IdReader,
+    class_info: &HashMap<Id, ClassDescription>,
+) -> Result<Vec<(Id, Id, FieldInfo, FieldValue)>, Error> {
+    let object_id: Id = id_reader.read_id(stream)?;
+    let stack_trace_serial: SerialNumber = stream.read_u32::<NetworkEndian>()?;
+    let class_object_id: Id = id_reader.read_id(stream)?;
+    let data_size: u64 = stream.read_u32::<NetworkEndian>()?.into();
+
+    let mut substream = stream.take(data_size);
+    let mut values = Vec::new();
+
+    // Read data class-by-class, going down into class hierarchy
+    let mut current_class_obj_id = class_object_id;
+    while Into::<u64>::into(current_class_obj_id) != 0 {
+        let class_desc: &ClassDescription = class_info
+            .get(&current_class_obj_id)
+            .ok_or(Error::UnknownClass(current_class_obj_id))?;
+
+        for field_info in class_desc.instance_fields.iter() {
+            let field_value: FieldValue =
+                read_type_value(&mut substream, field_info.field_type, id_reader)?;
+            values.push((object_id, current_class_obj_id, *field_info, field_value));
+        }
+
+        current_class_obj_id = class_desc.super_class_object_id;
+    }
+
+    Ok(values)
+}
+
+pub(crate) fn read_object_array<R: Read>(
+    stream: &mut R,
+    id_reader: IdReader,
+) -> Result<(Id, Id, Vec<Id>), Error> {
+    let object_id: Id = id_reader.read_id(stream)?;
+    let stack_trace_serial: SerialNumber = stream.read_u32::<NetworkEndian>()?;
+    let num_elements: usize = stream.read_u32::<NetworkEndian>()? as usize;
+    let element_class_id: Id = id_reader.read_id(stream)?;
+
+    let mut res = vec![Id::from(0 as u64); num_elements];
+
+    for elt in res.iter_mut() {
+        *elt = id_reader.read_id(stream)?;
+    }
+
+    Ok((object_id, element_class_id, res))
+}
+
+pub(crate) fn read_primitive_array<R: Read>(
+    stream: &mut R,
+    id_reader: IdReader,
+) -> Result<(Id, ArrayValue), Error> {
+    let object_id: Id = id_reader.read_id(stream)?;
+    let stack_trace_serial: SerialNumber = stream.read_u32::<NetworkEndian>()?;
+    let num_elemnts: usize = stream.read_u32::<NetworkEndian>()? as usize;
+    let elem_type: FieldType =
+        FieldType::try_from(stream.read_u8()?).or(Err(Error::InvalidField("ty")))?;
+
+    Ok((
+        object_id,
+        match elem_type {
+            FieldType::Object => return Err(Error::InvalidField("object type in primitive array")),
+            FieldType::Bool => {
+                let mut res: Vec<bool> = vec![false; num_elemnts];
+                for elt in res.iter_mut() {
+                    *elt = stream.read_u8()? != 0;
+                }
+                ArrayValue::Bool(res)
+            }
+            FieldType::Char => {
+                let mut res: Vec<u16> = vec![0; num_elemnts];
+                stream.read_u16_into::<NetworkEndian>(&mut res[..])?;
+                ArrayValue::Char(res)
+            }
+            FieldType::Float => {
+                let mut res: Vec<f32> = vec![0.0; num_elemnts];
+                stream.read_f32_into::<NetworkEndian>(&mut res[..])?;
+                ArrayValue::Float(res)
+            }
+            FieldType::Double => {
+                let mut res: Vec<f64> = vec![0.0; num_elemnts];
+                stream.read_f64_into::<NetworkEndian>(&mut res[..])?;
+                ArrayValue::Double(res)
+            }
+            FieldType::Byte => {
+                let mut res: Vec<i8> = vec![0; num_elemnts];
+                stream.read_i8_into(&mut res[..])?;
+                ArrayValue::Byte(res)
+            }
+            FieldType::Short => {
+                let mut res: Vec<i16> = vec![0; num_elemnts];
+                stream.read_i16_into::<NetworkEndian>(&mut res[..])?;
+                ArrayValue::Short(res)
+            }
+            FieldType::Int => {
+                let mut res: Vec<i32> = vec![0; num_elemnts];
+                stream.read_i32_into::<NetworkEndian>(&mut res[..])?;
+                ArrayValue::Int(res)
+            }
+            FieldType::Long => {
+                let mut res: Vec<i64> = vec![0; num_elemnts];
+                stream.read_i64_into::<NetworkEndian>(&mut res[..])?;
+                ArrayValue::Long(res)
+            }
+        },
+    ))
+}
+
+pub(crate) fn read_type_value<R: Read>(
+    substream: &mut R,
+    ty: FieldType,
+    id_reader: IdReader,
+) -> Result<FieldValue, Error> {
+    Ok(match ty {
+        FieldType::Object => FieldValue::Object(id_reader.read_id(substream)?),
+        FieldType::Bool => FieldValue::Bool(substream.read_u8()? != 0),
+        FieldType::Char => FieldValue::Char(substream.read_u16::<NetworkEndian>()?),
+        FieldType::Float => FieldValue::Float(substream.read_f32::<NetworkEndian>()?),
+        FieldType::Double => FieldValue::Double(substream.read_f64::<NetworkEndian>()?),
+        FieldType::Byte => FieldValue::Byte(substream.read_i8()?),
+        FieldType::Short => FieldValue::Short(substream.read_i16::<NetworkEndian>()?),
+        FieldType::Int => FieldValue::Int(substream.read_i32::<NetworkEndian>()?),
+        FieldType::Long => FieldValue::Long(substream.read_i64::<NetworkEndian>()?),
     })
 }
