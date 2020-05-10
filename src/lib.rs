3d78e7ec1670a552b2edc8b625a4b469b4799c75
Ivan Boldyrev <lispnik@gmail.com>
2020-05-10 16:47:38 +0300

    Cleanup lib.rs and add re-exports.

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,10 @@
 pub mod decl;
-pub mod reader;
-pub mod records;
+mod reader;
+mod records;
 pub mod stream;
-pub mod try_byteorder;
+mod try_byteorder;
 
 #[macro_use]
 extern crate static_assert_macro;
 
-use decl::{ClassRecord, HprofHeader};
-use std::collections::HashMap;
-
-pub struct Class {
-    pub record: ClassRecord,
-    pub unloaded: bool,
-}
-
-pub struct HprofReader<Str> {
-    pub header: HprofHeader<Str>,
-    pub strings: HashMap<u64, Str>,
-    pub classes: HashMap<u64, Class>,
-}
-
-impl<'a, Str: Clone + 'a> HprofReader<Str> {
-    pub fn from_stream_reader(header: &HprofHeader<Str>) -> Self {
-        Self {
-            header: header.clone(),
-            strings: HashMap::default(),
-            classes: HashMap::default(),
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+pub use stream::{MemoryHprofIterator, ReadHprofIterator, StreamHprofReader};
